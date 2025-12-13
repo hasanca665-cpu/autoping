7736dcb1826e871b189afee2d278245a1973a76e
hasanca665-cpu <hasanca665@gmail.com>
2025-12-12 18:42:31 -0600

    Update Wsck2.py

--- Wsck2.py
+++ Wsck2.py
@@ -146,7 +146,7 @@ class UltraFastBot:
     async def send_instant(self, client: UltraFastClient, phone: str):
         try:
             await client.client.send_message('@Sellws_bot', f"+1{phone}")
-            await asyncio.sleep(3.2)
+            await asyncio.sleep(0.5)
             msgs = await client.client.get_messages('@Sellws_bot', limit=12)
             for m in msgs:
                 if not m.out and (phone in m.message or f"+1{phone}" in m.message):
